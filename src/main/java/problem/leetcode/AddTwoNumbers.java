d73ee052a0856ced64f6f714622650f1e457c404
ErranderLee <dre12am14@gmail.com>
2023-01-04 01:16:52 +0900

    add two numbers

--- src/main/java/problem/leetcode/AddTwoNumbers.java
+++ src/main/java/problem/leetcode/AddTwoNumbers.java
@@ -2,20 +2,20 @@ package problem.leetcode;
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
  */
+class ListNode2 {
+     int val;
+     ListNode2 next;
+     ListNode2() {}
+     ListNode2(int val) { this.val = val; }
+     ListNode2(int val, ListNode2 next) { this.val = val; this.next = next; }
+}
 public class AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode2 addTwoNumbers(ListNode2 l1, ListNode2 l2) {
         int temp = 0;
         int overTen = 0;
-        ListNode answer = new ListNode();
-        ListNode pos = answer;
+        ListNode2 answer = new ListNode2();
+        ListNode2 pos = answer;
         temp = l1.val + l2.val;
         if (temp >= 10) {
             answer.val = temp - 10;
@@ -32,7 +32,7 @@ public class AddTwoNumbers {
                 overTen = 1;
                 temp -= 10;
             }
-            pos.next = new ListNode(temp);
+            pos.next = new ListNode2(temp);
             pos = pos.next;
             l1 = l1.next;
             l2 = l2.next;
@@ -46,7 +46,7 @@ public class AddTwoNumbers {
             } else {
                 overTen = 0;
             }
-            pos.next = new ListNode(temp);
+            pos.next = new ListNode2(temp);
             pos = pos.next;
             l1 = l1.next;
         }
@@ -59,13 +59,13 @@ public class AddTwoNumbers {
             } else {
                 overTen = 0;
             }
-            pos.next = new ListNode(temp);
+            pos.next = new ListNode2(temp);
             pos = pos.next;
             l2 = l2.next;
         }
 
         if (overTen == 1) {
-            pos.next = new ListNode(1);
+            pos.next = new ListNode2(1);
         }
 
         return answer;
