32d1a72ed2d79f972ab8ed3e67a93f34e11e082d
ErranderLee <dre12am14@gmail.com>
2023-02-08 21:16:57 +0900

    ps

--- src/main/java/problem/boj/Boj2491.java
+++ src/main/java/problem/boj/Boj2491.java
@@ -47,10 +47,42 @@ public class Boj2491 {
         return answer;
     }
 
+    static int solutionUsingDp(int[] nums) {
+        int pos = 0;
+        int answer = 1;
+        int[] dpAsc = new int[nums.length];
+        dpAsc[0] = 1;
+        int[] dpDsc = new int[nums.length];
+        dpDsc[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[pos] <= nums[i]) {
+                dpAsc[i] = dpAsc[i - 1] + 1;
+            } else {
+                dpAsc[i] = 1;
+            }
+            pos += 1;
+            answer = Math.max(dpAsc[i], answer);
+        }
+
+        pos = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[pos] >= nums[i]) {
+                dpDsc[i] = dpDsc[i - 1] + 1;
+            } else {
+                dpDsc[i] = 1;
+            }
+            pos += 1;
+            answer = Math.max(dpDsc[i], answer);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         int[] numsStr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        System.out.println(solution(numsStr));
+        System.out.println(solutionUsingDp(numsStr));
     }
 }
